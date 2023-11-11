02ac37ab264eeefc9f0719a1fd2585fe5ea57863
J. Cliff Dyer <jcd@sdf.org>
2023-11-11 09:55:21 -0500

    Add accessors, predicates, and parsing functions, and some constructors

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@
 #[repr(C)]
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct tg_point {
-    pub x: f64,
-    pub y: f64,
+    pub x: libc::c_double,
+    pub y: libc::c_double,
 }
 
 /// The base segment type used in tg_line and tg_ring for joining two vertices.
@@ -19,7 +19,7 @@ pub struct tg_segment {
 }
 
 /// A rectangle defined by a minimum and maximum coordinates.
-/// Returned by the tg_geom_rect(), tg_ring_rect(), and other \*_rect() 
+/// Returned by the tg_geom_rect(), tg_ring_rect(), and other \*_rect()
 /// functions for getting a geometry's minumum bounding rectangle.
 /// Also used internally for geometry indexing.
 /// @see RectFuncs
@@ -40,36 +40,69 @@ pub struct tg_rect {
 #[repr(usize)]
 pub enum tg_geom_type {
     ///< Point
-    TG_POINT              = 1, 
+    TG_POINT = 1,
     ///< LineString
-    TG_LINESTRING         = 2,
+    TG_LINESTRING = 2,
     ///< Polygon
-    TG_POLYGON            = 3,
+    TG_POLYGON = 3,
     ///< MultiPoint, collection of points
-    TG_MULTIPOINT         = 4,
+    TG_MULTIPOINT = 4,
     ///< MultiLineString, collection of linestrings
-    TG_MULTILINESTRING    = 5,
+    TG_MULTILINESTRING = 5,
     ///< MultiPolygon, collection of polygons
-    TG_MULTIPOLYGON       = 6,
+    TG_MULTIPOLYGON = 6,
     ///< GeometryCollection, collection of geometries
     TG_GEOMETRYCOLLECTION = 7,
 }
 
-///< Find the description in the tg.c file.
-pub enum tg_line {}  
-///< Find the description in the tg.c file.
-pub enum tg_ring {}  
-///< Find the description in the tg.c file.
-pub enum tg_poly {}  
-///< Find the description in the tg.c file.
-pub enum tg_geom {}  
+/// Geometry indexing options.
+///
+/// Used for polygons, rings, and lines to make the point-in-polygon and
+/// geometry intersection operations fast.
+///
+/// An index can also be used for efficiently traversing, searching, and
+/// performing nearest-neighbor (kNN) queries on the segment using
+/// tg_ring_index_*() and tg_ring_nearest() functions.
+#[repr(C)]
+pub enum tg_index {
+    ///< default is TG_NATURAL or tg_env_set_default_index().
+    TG_DEFAULT,
+    ///< no indexing available, or disabled.
+    TG_NONE,
+    ///< indexing with natural ring order, for rings/lines
+    TG_NATURAL,
+    ///< indexing using segment striping, rings only
+    TG_YSTRIPES,
+}
 
-/// Functions for creating and freeing geometries. 
+///< Find the description in the tg.c file.
+pub enum tg_line {}
+///< Find the description in the tg.c file.
+pub enum tg_ring {}
+///< Find the description in the tg.c file.
+pub enum tg_poly {}
+///< Find the description in the tg.c file.
+pub enum tg_geom {}
+
+/// Functions for creating and freeing geometries.
 pub mod GeometryConstructors {
-    use super::tg_geom;
+    use crate::tg_geom;
+    use crate::{tg_line, tg_point, tg_poly};
+
     #[link(name = "tg")]
-    extern {
+    extern "C" {
+        pub fn tg_geom_new_point(point: tg_point) -> *mut tg_geom;
+        pub fn tg_geom_new_linestring(line: *const tg_line) -> *mut tg_geom;
+        pub fn tg_geom_new_polygon(poly: *const tg_poly) -> *mut tg_geom;
+        pub fn tg_geom_new_multipoint(
+            points: *const tg_point,
+            npoints: libc::c_int,
+        ) -> *mut tg_geom;
+        // ...
+        pub fn tg_geom_clone(geom: *const tg_geom) -> *mut tg_geom;
+        pub fn tg_geom_copy(geom: *const tg_geom) -> *mut tg_geom;
         pub fn tg_geom_free(geom: *mut tg_geom);
+
     }
 
     /*
@@ -86,12 +119,79 @@ pub mod GeometryConstructors {
     */
 }
 
+/// Geometry accessors
+///
+/// Functions for accessing various information about geometries, such as
+/// getting the geometry type or extracting underlying components or
+/// coordinates.
+pub mod GeometryAccessors {
+    use crate::{tg_geom_type, tg_geom, tg_rect, tg_point, tg_line, tg_poly};
+
+    extern "C" {
+        pub fn tg_geom_typeof(geom: *const tg_geom) -> tg_geom_type;
+        pub fn tg_geom_type_string(r#type: tg_geom_type) -> *const libc::c_char;
+        pub fn tg_geom_rect(geom: *const tg_geom) -> tg_rect;
+        pub fn tg_geom_is_feature(geom: *const tg_geom) -> bool;
+        pub fn tg_geom_is_featurecollection(geom: *const tg_geom) -> bool;
+        pub fn tg_geom_point(geom: *const tg_geom) -> tg_point;
+        pub fn tg_geom_line(geom: *const tg_geom) -> *const tg_line;
+        pub fn tg_geom_poly(geom: *const tg_geom) -> *const tg_poly;
+        pub fn tg_geom_num_points(geom: *const tg_geom) -> libc::c_int;
+        pub fn tg_geom_point_at(geom: *const tg_geom, index: libc::c_int) -> tg_point;
+        pub fn tg_geom_num_lines(geom: *const tg_geom) -> libc::c_int;
+        pub fn tg_geom_line_at(geom: *const tg_geom, index: libc::c_int)-> *const tg_line ;
+        pub fn tg_geom_num_polys(geom: *const tg_geom) -> libc::c_int;
+        pub fn tg_geom_poly_at(geom: *const tg_geom, index: libc::c_int)-> *const tg_poly ;
+        pub fn tg_geom_num_geometries(geom: *const tg_geom) -> libc::c_int;
+        pub fn tg_geom_geometry_at(geom: *const tg_geom, index: libc::c_int)-> *const tg_geom ;
+        pub fn tg_geom_extra_json(geom: *const tg_geom) -> *const libc::c_char;
+        pub fn tg_geom_is_empty(geom: *const tg_geom) -> bool;
+        pub fn tg_geom_dims(geom: *const tg_geom) -> libc::c_int;
+        pub fn tg_geom_has_z(geom: *const tg_geom) -> bool;
+        pub fn tg_geom_has_m(geom: *const tg_geom) -> bool;
+        pub fn tg_geom_z(geom: *const tg_geom) -> libc::c_double;
+        pub fn tg_geom_m(geom: *const tg_geom) -> libc::c_double;
+        pub fn tg_geom_extra_coords(geom: *const tg_geom) -> *const libc::c_double;
+        pub fn tg_geom_num_extra_coords(geom: *const tg_geom) -> libc::c_int;
+        pub fn tg_geom_memsize(geom: *const tg_geom) -> libc::size_t;
+        pub fn tg_geom_search(
+            geom: *const tg_geom,
+            rect: tg_rect,
+            iter: extern "C" fn(geom: *const tg_geom, index: libc::c_int, udata: *mut libc::c_void) -> bool,
+            udata: *mut libc::c_void,
+        );
+    // pub fn tg_geom_search
+    }
+
+    /*
+    void tg_geom_search(const struct tg_geom *geom, struct tg_rect rect,
+        bool (*iter)(const struct tg_geom *geom, int index, void *udata),
+        void *udata);
+    */
+}
+/// GeometryPredicates
+///
 /// Functions for testing the spatial relations of two geometries.
 pub mod GeometryPredicates {
-    use super::tg_geom;
+
+    use crate::tg_geom;
+    use crate::tg_rect;
+
     #[link(name = "tg")]
-    extern {
+    extern "C" {
+        pub fn tg_geom_equals(a: *const tg_geom, b: *const tg_geom) -> bool;
         pub fn tg_geom_intersects(a: *const tg_geom, b: *const tg_geom) -> bool;
+        pub fn tg_geom_disjoint(a: *const tg_geom, b: *const tg_geom) -> bool;
+        pub fn tg_geom_contains(a: *const tg_geom, b: *const tg_geom) -> bool;
+        pub fn tg_geom_within(a: *const tg_geom, b: *const tg_geom) -> bool;
+        pub fn tg_geom_covers(a: *const tg_geom, b: *const tg_geom) -> bool;
+        pub fn tg_geom_coveredby(a: *const tg_geom, b: *const tg_geom) -> bool;
+        pub fn tg_geom_intersects_rect(a: *const tg_geom, b: tg_rect) -> bool;
+        pub fn tg_geom_intersects_xy(
+            a: *const tg_geom,
+            x: libc::c_double,
+            y: libc::c_double,
+        ) -> bool;
     }
     /*
     bool tg_geom_equals(const struct tg_geom *a, const struct tg_geom *b);
@@ -107,46 +207,40 @@ pub mod GeometryPredicates {
     */
 }
 
+/// Geometry Parsing
+///
 /// Functions for parsing geometries from external data representations.
 /// It's recommended to use tg_geom_error() after parsing to check for errors.
 pub mod GeometryParsing {
-    use std::ffi::c_char;
+    use crate::{tg_geom, tg_index};
 
-    use super::tg_geom;
     #[link(name = "tg")]
-    extern {
-        pub fn tg_parse_geojson(geojson: *const c_char) -> *mut tg_geom;
-        pub fn tg_parse_wkt(wkt: *const c_char) -> *mut tg_geom; 
-
-        // ...
-        pub fn tg_geom_error(geom: *const tg_geom) -> *const c_char;
+    extern "C" {
+        pub fn tg_parse_geojson(geojson: *const libc::c_char) -> *mut tg_geom;
+        pub fn tg_parse_geojsonn(geojson: *const libc::c_char, len: libc::size_t) -> *mut tg_geom;
+        pub fn tg_parse_geojson_ix(geojson: *const libc::c_char, ix: tg_index) -> *mut tg_geom;
+        pub fn tg_parse_geojsonn_ix(geojson: *const libc::c_char, len: libc::size_t, ix: tg_index) -> *mut tg_geom;
+        pub fn tg_parse_wkt(wkt: *const libc::c_char) -> *mut tg_geom;
+        pub fn tg_parse_wktn(wkt: *const libc::c_char, len: libc::size_t) -> *mut tg_geom;
+        pub fn tg_parse_wkt_ix(wkt: *const libc::c_char, ix: tg_index) -> *mut tg_geom;
+        pub fn tg_parse_wktn_ix(wkt: *const libc::c_char, len: libc::size_t, ix: tg_index) -> *mut tg_geom;
+        pub fn tg_parse_wkb(wkb: *const u8, len: libc::size_t) -> *mut tg_geom;
+        pub fn tg_parse_wkb_ix(wkb: *const u8, len: libc::size_t, ix: tg_index) -> *mut tg_geom;
+        pub fn tg_parse_hex(hex: *const libc::c_char) -> *mut tg_geom;
+        pub fn tg_parse_hexn(hex: *const libc::c_char, len: libc::size_t) -> *mut tg_geom;
+        pub fn tg_parse_hex_ix(hex: *const libc::c_char, ix: tg_index) -> *mut tg_geom;
+        pub fn tg_parse_hexn_ix(hex: *const libc::c_char, len: libc::size_t, ix: tg_index) -> *mut tg_geom;
+        pub fn tg_geom_error(geom: *const tg_geom) -> *const libc::c_char;
     }
-    /*
-    struct tg_geom *tg_parse_geojson(const char *geojson);
-    struct tg_geom *tg_parse_geojsonn(const char *geojson, size_t len);
-    struct tg_geom *tg_parse_geojson_ix(const char *geojson, enum tg_index ix);
-    struct tg_geom *tg_parse_geojsonn_ix(const char *geojson, size_t len, enum tg_index ix);
-    struct tg_geom *tg_parse_wkt(const char *wkt);
-    struct tg_geom *tg_parse_wktn(const char *wkt, size_t len);
-    struct tg_geom *tg_parse_wkt_ix(const char *wkt, enum tg_index ix);
-    struct tg_geom *tg_parse_wktn_ix(const char *wkt, size_t len, enum tg_index ix);
-    struct tg_geom *tg_parse_wkb(const uint8_t *wkb, size_t len);
-    struct tg_geom *tg_parse_wkb_ix(const uint8_t *wkb, size_t len, enum tg_index ix);
-    struct tg_geom *tg_parse_hex(const char *hex);
-    struct tg_geom *tg_parse_hexn(const char *hex, size_t len);
-    struct tg_geom *tg_parse_hex_ix(const char *hex, enum tg_index ix);
-    struct tg_geom *tg_parse_hexn_ix(const char *hex, size_t len, enum tg_index ix);
-    */
 }
 
-/// @}
-/// Point functions
+/// Point Functions
 ///
 /// Functions for working directly with the tg_point type.
 pub mod PointFuncs {
     use crate::{tg_point, tg_rect};
     #[link(name = "tg")]
-    extern {
+    extern "C" {
         pub fn tg_point_rect(point: tg_point) -> tg_rect;
         pub fn tg_point_intersects_rect(point: tg_point, rect: tg_rect) -> bool;
     }
@@ -158,7 +252,7 @@ pub mod PointFuncs {
 pub mod SegmentFuncs {
     use crate::{tg_rect, tg_segment};
     #[link(name = "tg")]
-    extern {
+    extern "C" {
         pub fn tg_segment_rect(s: tg_segment) -> tg_rect;
         pub fn tg_segment_intersects_segment(a: tg_segment, b: tg_segment);
     }
@@ -173,7 +267,7 @@ pub mod SegmentFuncs {
 // https://github.com/tidwall/tg
 //
 // Copyright 2023 Joshua J Baker. All rights reserved.
-// Use of this source code is governed by a license 
+// Use of this source code is governed by a license
 // that can be found in the LICENSE file.
 
 #ifndef TG_H
@@ -185,24 +279,11 @@ pub mod SegmentFuncs {
 
 
 
-/// Geometry indexing options.
-///
-/// Used for polygons, rings, and lines to make the point-in-polygon and
-/// geometry intersection operations fast.
-///
-/// An index can also be used for efficiently traversing, searching, and 
-/// performing nearest-neighbor (kNN) queries on the segment using 
-/// tg_ring_index_*() and tg_ring_nearest() functions.
-enum tg_index { 
-    TG_DEFAULT,  ///< default is TG_NATURAL or tg_env_set_default_index().
-    TG_NONE,     ///< no indexing available, or disabled.
-    TG_NATURAL,  ///< indexing with natural ring order, for rings/lines
-    TG_YSTRIPES, ///< indexing using segment striping, rings only
-};
 
 /// @defgroup GeometryConstructors Geometry constructors
 /// @}
 
+/// @defgroup GeometryAccessors Geometry accessors
 /// @defgroup GeometryAccessors Geometry accessors
 /// Functions for accessing various information about geometries, such as
 /// getting the geometry type or extracting underlying components or
@@ -307,7 +388,7 @@ bool tg_rect_intersects_point(struct tg_rect a, struct tg_point b);
 /// Functions for working directly with the tg_ring type.
 ///
 /// There are no direct spatial predicates for tg_ring.
-/// If you want to perform operations like "intersects" or "covers" then you 
+/// If you want to perform operations like "intersects" or "covers" then you
 /// must upcast the ring to a tg_geom, like such:
 ///
 /// ```
@@ -332,17 +413,17 @@ int tg_ring_index_spread(const struct tg_ring *ring);
 int tg_ring_index_num_levels(const struct tg_ring *ring);
 int tg_ring_index_level_num_rects(const struct tg_ring *ring, int levelidx);
 struct tg_rect tg_ring_index_level_rect(const struct tg_ring *ring, int levelidx, int rectidx);
-bool tg_ring_nearest_segment(const struct tg_ring *ring, 
+bool tg_ring_nearest_segment(const struct tg_ring *ring,
     double (*rect_dist)(struct tg_rect rect, int *more, void *udata),
     double (*seg_dist)(struct tg_segment seg, int *more, void *udata),
     bool (*iter)(struct tg_segment seg, double dist, int index, void *udata),
     void *udata);
-void tg_ring_line_search(const struct tg_ring *a, const struct tg_line *b, 
-    bool (*iter)(struct tg_segment aseg, int aidx, struct tg_segment bseg, 
+void tg_ring_line_search(const struct tg_ring *a, const struct tg_line *b,
+    bool (*iter)(struct tg_segment aseg, int aidx, struct tg_segment bseg,
         int bidx, void *udata),
     void *udata);
-void tg_ring_ring_search(const struct tg_ring *a, const struct tg_ring *b, 
-    bool (*iter)(struct tg_segment aseg, int aidx, struct tg_segment bseg, 
+void tg_ring_ring_search(const struct tg_ring *a, const struct tg_ring *b,
+    bool (*iter)(struct tg_segment aseg, int aidx, struct tg_segment bseg,
         int bidx, void *udata),
     void *udata);
 double tg_ring_area(const struct tg_ring *ring);
@@ -353,7 +434,7 @@ double tg_ring_perimeter(const struct tg_ring *ring);
 /// Functions for working directly with the tg_line type.
 ///
 /// There are no direct spatial predicates for tg_line.
-/// If you want to perform operations like "intersects" or "covers" then you 
+/// If you want to perform operations like "intersects" or "covers" then you
 /// must upcast the line to a tg_geom, like such:
 ///
 /// ```
@@ -377,13 +458,13 @@ int tg_line_index_spread(const struct tg_line *line);
 int tg_line_index_num_levels(const struct tg_line *line);
 int tg_line_index_level_num_rects(const struct tg_line *line, int levelidx);
 struct tg_rect tg_line_index_level_rect(const struct tg_line *line, int levelidx, int rectidx);
-bool tg_line_nearest_segment(const struct tg_line *line, 
+bool tg_line_nearest_segment(const struct tg_line *line,
     double (*rect_dist)(struct tg_rect rect, int *more, void *udata),
     double (*seg_dist)(struct tg_segment seg, int *more, void *udata),
     bool (*iter)(struct tg_segment seg, double dist, int index, void *udata),
     void *udata);
-void tg_line_line_search(const struct tg_line *a, const struct tg_line *b, 
-    bool (*iter)(struct tg_segment aseg, int aidx, struct tg_segment bseg, 
+void tg_line_line_search(const struct tg_line *a, const struct tg_line *b,
+    bool (*iter)(struct tg_segment aseg, int aidx, struct tg_segment bseg,
         int bidx, void *udata),
     void *udata);
 double tg_line_length(const struct tg_line *line);
@@ -393,7 +474,7 @@ double tg_line_length(const struct tg_line *line);
 /// Functions for working directly with the tg_poly type.
 ///
 /// There are no direct spatial predicates for tg_poly.
-/// If you want to perform operations like "intersects" or "covers" then you 
+/// If you want to perform operations like "intersects" or "covers" then you
 /// must upcast the poly to a tg_geom, like such:
 ///
 /// ```
